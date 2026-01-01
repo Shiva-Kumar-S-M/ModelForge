b1b402519405cc6b2abcccd92e7094ceeb81a460
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2026-01-01 11:00:00 +0530

    Data preprocessing

---
+++ src/data_preprocessing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def drop_unnecessary_columns(df):
+    columns_to_drop=['PassengerId', 'Name', 'Ticket', 'Cabin']
+    df=df.drop(columns=columns_to_drop)
+    return df
+
+
+
+#Handling missinf values
+def handle_missing_values(df):
+    df['Age']=df['Age'].fillna(df['Age'].median())
+    df['Embarked']=df['Embarked'].fillna(df['Embarked'].mode()[0])
+    return df
+
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from data_preprocessing import drop_unnecessary_columns,handle_missing_values
 
 
 def load_data(path):
@@ -12,11 +13,17 @@ if __name__ == "__main__":
 
     data = load_data("data/raw/train.csv")
 
-    print("First 5 rows:")
+    # print("First 5 rows:")
+    # print(data.head())
+
+    # print("\nDataset shape:")
+    # print(data.shape)
+
+    # print("\nColumns:")
+    # print(data.columns)
+
+    data=drop_unnecessary_columns(data)
+
+    data=handle_missing_values(data)
+
     print(data.head())
-
-    print("\nDataset shape:")
-    print(data.shape)
-
-    print("\nColumns:")
-    print(data.columns)
